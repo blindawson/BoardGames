28e90f5d893918872758b7162132215023b73484
Brian Lindawson <brlw@cowi.com>
2020-05-01 17:14:24 -0700

    Continue to build Can't Stop

--- BoardGames/CantStop/CantStop.py
+++ BoardGames/CantStop/CantStop.py
@@ -3,38 +3,170 @@ import numpy as np
 from itertools import combinations
 
 
-def dice_df():
+class Dice:
+    def __init__(self, d1=None, d2=None, d3=None, d4=None):
+        self.values = pd.Series([d1, d2, d3, d4])
+
+        def set_rand(d):
+            if d is None:
+                d = np.random.randint(1, 7)
+            return d
+        self.values = self.values.apply(lambda d: set_rand(d))
+
+    def __repr__(self):
+        return f'{self.values[0]}, {self.values[1]}, {self.values[2]}, {self.values[3]}'
+
+    def roll_dice(self):
+        self.values = self.values.apply(lambda d: np.random.randint(1, 7))
+        return self
+
+    def pair(self, verbose=False):
+        d01 = self.values[0] + self.values[1]
+        d02 = self.values[0] + self.values[2]
+        d03 = self.values[0] + self.values[3]
+        d12 = self.values[1] + self.values[2]
+        d13 = self.values[1] + self.values[3]
+        d23 = self.values[2] + self.values[3]
+        # TODO Would be nice to print lowest to highest
+        if verbose:
+            print(f'{d01} and {d23}\n'
+                  f'{d02} and {d13}\n'
+                  f'{d03} and {d12}')
+        return [[d01, d23], [d02, d13], [d03, d12]]
+
+
+class Board:
+    def __init__(self, *args):
+        if len(args) == 0:
+            args = ('p1', 'p2')
+        self.col_len = pd.Series([3, 5, 7, 9, 11, 13, 11, 9, 7, 5, 3],
+                                 index=range(2, 13), name='Column Length')
+        self.df = pd.DataFrame(0, index=self.col_len.index, columns=list(args[0]))
+        self.df.index.name = 'Column Number'
+        self.df['Column Length'] = self.col_len
+        self.df['Locked'] = False
+
+    def move_runner(self, player, column_number):
+        if self.df.loc[column_number, player] == self.df.loc[column_number, 'Column Length']:
+            raise Exception('Runner cannot move any higher.')
+        self.df.loc[column_number, player] += 1
+        player.columns[column_number] += 1
+
+    def lock_column(self, column_number):
+        if self.df.loc[column_number, 'Locked']:
+            raise Exception('Column is already locked.')
+        self.df.loc[column_number, 'Locked'] = True
+
+    # TODO add __repr__. It'd be extra cool if this could be a figure
+
+
+class Player:
+    def __init__(self, name='Anonymous'):
+        self.score = 0
+        self.columns = pd.Series(0, index=range(2, 13), name='Column Progress')
+        self.name = name
+
+    def __repr__(self):
+        return (f'{self.name}\n'
+                f'Score: {self.score}\n'
+                f'{self.columns.to_frame().T}')
+
+
+class Turn:
+    def __init__(self, player, board):
+        self.player = player
+        self.board = board
+        self.dice = Dice()
+        print('taking a turn')
+        self.start_turn()
+        self.roll()
+        self.resolve_roll()
+        self.ask_continue()
+
+    def start_turn(self):
+        print(f"{self.player.name}'s Turn")
+
+    def roll(self):
+        self.dice = self.dice.roll_dice()
+        print(f'{self.player.name} rolled: {self.dice}\n')
+
+    def resolve_roll(self):
+        pass
+
+    def choose_dice(self):
+        print(f'Your options are:')
+        self.dice.pair()
+        option = int(input('Choose pair 1, 2, or 3:'))
+        # self.player.columns[dice_.pair()[option - 1]] += 1
+
+    def ask_continue(self):
+        pass
+
+
+class Game:
+    def __init__(self, *args):
+        print(type(list(args)))
+        self.board = Board(args)
+        self.players = []
+        for player in args:
+            if isinstance(player, str):
+                self.players.append(Player(player))
+            elif isinstance(player, Player):
+                self.players.append(player)
+        self.starting_player = np.random.choice(self.players)
+        Turn(self.starting_player, self.board)
+
+
+    # TODO Check that Num players is 2 to 4
+    # TODO Choose first player
+    # TODO Make a turn class and have the first player take their turn
+    # TODO Alternate Players Turn
+    # TODO Check end of turn
+    # TODO Dice pair verbose should default to false
+
+
+def all_dice_combinations():
     df = pd.DataFrame(
         np.array(
-            np.meshgrid(range(1,7), range(1,7),
-                        range(1,7), range(1,7)))
+            np.meshgrid(range(1, 7), range(1, 7),
+                        range(1, 7), range(1, 7)))
         .reshape(4, -1).T,
-        columns=['D1', 'D2', 'D3', 'D4'])
-    df['D12'] = df['D1'] + df['D2']
-    df['D13'] = df['D1'] + df['D3']
-    df['D14'] = df['D1'] + df['D4']
+        columns=['D0', 'D1', 'D2', 'D3'])
+    df['D01'] = df['D0'] + df['D1']
     df['D23'] = df['D2'] + df['D3']
-    df['D24'] = df['D2'] + df['D4']
-    df['D34'] = df['D3'] + df['D4']
+    df['D02'] = df['D0'] + df['D2']
+    df['D13'] = df['D1'] + df['D3']
+    df['D03'] = df['D0'] + df['D3']
+    df['D12'] = df['D1'] + df['D2']
     return df
 
 
-def odds(n1, n2, n3):
-    dice = dice_df()
+def odds(*args):
     hits = pd.DataFrame([])
-    hits['n1'] = (dice.loc[:, 'D12':'D34'] == n1).any(axis=1)
-    hits['n2'] = (dice.loc[:, 'D12':'D34'] == n2).any(axis=1)
-    hits['n3'] = (dice.loc[:, 'D12':'D34'] == n3).any(axis=1)
-    hits['any'] = hits.loc[:,'n1':'n3'].any(axis=1)
+    for i, num in enumerate(args):
+        hits[i] = (all_dice_combinations().loc[:, 'D01':'D12'] == num).any(axis=1)
+    hits['any'] = hits.loc[:, 0:len(args)].any(axis=1)
     return hits['any'].sum() / len(hits)
 
 
 def odds_table():
-    df = pd.DataFrame(list(combinations(range(2,13), 3)),
+    df = pd.DataFrame(list(combinations(range(2, 13), 3)),
                       columns=['N1', 'N2', 'N3'])
     df['Odds'] = df.apply(lambda r: odds(r['N1'], r['N2'], r['N3']), axis=1)
     return df
 
 
-# How much is there to gain by hitting on one or two of three numbers.
-def benefit():
+def progress_value(row, position=0):
+    total_spaces = Board().col_len[row]
+    remaining_spaces = total_spaces - position
+    if remaining_spaces == 0:
+        return 0
+    else:
+        return 1 / remaining_spaces
+
+
+
+
+# TODO turn counter
+# TODO bust
+
--- tests/CantStop/test_cantstop.py
+++ tests/CantStop/test_cantstop.py
@@ -1,9 +1,59 @@
 import pandas as pd
-import pathlib
+import numpy as np
 import pytest
 from BoardGames.CantStop import CantStop
 
+odds_table = pd.read_csv('odds_table.csv', header=0, index_col=0)
+dice_df = pd.read_csv('dice_df.csv', header=0, index_col=0)
 
-def test_peak_outputs():
-    odds_table = pd.read_csv('odds_table.csv', header=0, index_col=0)
+
+def test_dice_class():
+    dice = CantStop.Dice(1, 2, 5, 6)
+    assert dice.values[0] == 1
+    assert dice.values[1] == 2
+    assert dice.values[2] == 5
+    assert dice.values[3] == 6
+
+
+def test_board_class():
+    board = CantStop.Board('p1', 'p2', 'p3', 'p4')
+    assert board.col_len[9] == 9
+
+
+def test_pair_dice():
+    dice = CantStop.Dice(1, 2, 5, 6)
+    assert dice.pair() == [[3, 11], [6, 8], [7, 7]]
+
+
+def test_all_dice_combinations():
+    assert CantStop.all_dice_combinations().values == pytest.approx(dice_df.values)
+
+
+def test_odds_table():
     assert CantStop.odds_table().values == pytest.approx(odds_table.values)
+
+
+def test_progress_value():
+    assert CantStop.progress_value(3, 1) == 0.25
+    assert CantStop.progress_value(8, 5) == 1 / 6
+    assert CantStop.progress_value(11, 0) == 1 / 5
+    assert CantStop.progress_value(12, 3) == 0
+
+
+# def test_game_init():
+#     assert CantStop.Game('p1', 'p2', 'p3').players == [CantStop.Player('p1'),
+#                                                        CantStop.Player('p2'),
+#                                                        CantStop.Player('p3')]
+
+
+def test_odds():
+    assert CantStop.odds(6, 7, 8) == 0.9197530864197531
+    assert CantStop.odds(2, 4, 5) == 0.6574074074074074
+    assert CantStop.odds(9, 3, 8) == 0.8356481481481481
+
+# def test_choose_rows():
+#     assert CantStop.choose_rows(d1=1, d2=2, d3=3, d4=4) ==
+#     assert CantStop.choose_rows(d1=5, d2=3, d3)
+
+# TODO Some tests like test_all_dice_combinations prints a ton to the output.
+#  Where is that coming from and how do I remove it?
