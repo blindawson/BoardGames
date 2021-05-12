e9cf5bd7583f14c5d4a236cea7b0145198057938
Brian Lindawson <brlw@cowi.com>
2021-05-12 09:02:12 -0700

    Adding sections to print odds that each player get each faction

--- BoardGames/Root/Choose_Factions.py
+++ BoardGames/Root/Choose_Factions.py
@@ -1,22 +1,48 @@
+import numpy as np
 from BoardGames.Root import Randomizer
 
-lindawson_history = Randomizer.read_history('C')
-fiendfellow_history = Randomizer.read_history('F')
-rootaphile_history = Randomizer.read_history('I')
-bdlocas_history = Randomizer.read_history('L')
+lindawson_h, fiendfellow_h, rootaphile_h, bdlocas_h = Randomizer.read_history()
 
-lindawson = Randomizer.Player('Lindawson', history=lindawson_history)
-fiendfellow = Randomizer.Player('FiendFellow', history=fiendfellow_history)
-rootaphile = Randomizer.Player('Rootaphile', history=rootaphile_history)
-bdlocas = Randomizer.Player('bdlocas', history=bdlocas_history)
+lindawson = Randomizer.Player('Lindawson', history=lindawson_h)
+fiendfellow = Randomizer.Player('FiendFellow', history=fiendfellow_h)
+rootaphile = Randomizer.Player('Rootaphile', history=rootaphile_h)
+bdlocas = Randomizer.Player('bdlocas', history=bdlocas_h)
+players = [lindawson, fiendfellow, rootaphile, bdlocas]
 
-print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
-# print(Randomizer.randomize([lindawson, fiendfellow, rootaphile, bdlocas]))
+n = 10000
+analysis = [Randomizer.randomize(players) for i in range(n)]
+
+
+def faction_chance(results, faction):
+    return round(sum(faction in r for r in results)/len(results)*100)
+
+
+def double_vagabond_chance(results):
+    return round(sum((r.count('Vagabond') == 2) for r in results)/len(results)*100)
+
+
+print('Chance of each faction being in the game:')
+for f in Randomizer.all_factions:
+    print(f'{f}: {faction_chance(analysis, f)}%')
+print(f'Double Vagabonds: {double_vagabond_chance(analysis)}%')
+
+
+def print_player_odds(results, player):
+    player_order = {'Lindawson': 0, 'FiendFellow': 1, 'Rootaphile': 2, 'bdlocas': 3}
+    player_row = np.array(results)[:, player_order[player.name]]
+    print(f'\n{player} Actual Odds:')
+    for fa in Randomizer.all_factions:
+        print(f'{fa}: {round(np.sum(player_row == fa)/len(player_row)*100)}%')
+
+
+def print_player_odds_original(player):
+    print(f'\n{player} Original Odds:')
+    for fa in Randomizer.all_factions:
+        print(f'{fa}: {round(player.options.count(fa)/len(player.options)*100)}%')
+
+
+for p in players:
+    print_player_odds_original(p)
+    print_player_odds(analysis, p)
+
+print(Randomizer.randomize(players))
